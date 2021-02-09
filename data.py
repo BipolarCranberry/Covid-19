48d3a51d21c7fd5d3633450550d110041fb7cdea
Bipolar Cranberry <tim.fleischmann@gmail.com>
2021-02-09 15:03:50 +0100

    added create_time_series

--- data.py
+++ data.py
@@ -1,7 +1,23 @@
 import urllib.request
+import csv
+import pandas as pd
+
+class data():
+
+    def __init__(self):
+        data.time_series=0
+
+    def update_csv(self):
+        url = 'https://raw.githubusercontent.com/Daniel-Breuss/covid-data-austria/master/austriadata.csv'
+        urllib.request.urlretrieve(url, './Data/data-austria.csv')
+
+    def create_time_series(self):
+        self.time_series=pd.read_csv("./Data/data-austria.csv",usecols=["Datum","Fälle_gesamt"])
+        
+data_austria=data()
+data_austria.create_time_series()
+print(data_austria.time_series)
 
 
-def update_data():
-    url = 'https://raw.githubusercontent.com/Daniel-Breuss/covid-data-austria/master/austriadata.csv'
-    urllib.request.urlretrieve(url, './Data/data-austria.csv')
+
 
